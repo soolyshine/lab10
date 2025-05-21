a3461217004e189d320a7e996335728edf3e3a98
soolyshine <anna.krasnobaieva@infiz.khpi.edu.ua>
2025-05-21 17:30:09 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import UserList from './components/UserList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Users from API</h1>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
